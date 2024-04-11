92db7910adea7e144c767e52da5fe3350dd830fd
Ayushi Prasad <ayushiprasad850@gmail.com>
2024-04-12 00:05:34 +0530

    added volunteer schema

---
+++ backend/dist/volunteer/volunteer.schema.js
@@ -0,0 +1,73 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VolunteerSchema = exports.Volunteer = void 0;
+const mongoose_1 = require("@nestjs/mongoose");
+let Volunteer = class Volunteer {
+};
+exports.Volunteer = Volunteer;
+__decorate([
+    (0, mongoose_1.Prop)({ required: true }),
+    __metadata("design:type", String)
+], Volunteer.prototype, "_id", void 0);
+__decorate([
+    (0, mongoose_1.Prop)({ required: true }),
+    __metadata("design:type", Object)
+], Volunteer.prototype, "userId", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Volunteer.prototype, "profilePhoto", void 0);
+__decorate([
+    (0, mongoose_1.Prop)({ required: true }),
+    __metadata("design:type", String)
+], Volunteer.prototype, "name", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Volunteer.prototype, "phoneNo", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Volunteer.prototype, "city", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Volunteer.prototype, "state", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Volunteer.prototype, "zipcode", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Array)
+], Volunteer.prototype, "daysOfWeekAvailable", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Array)
+], Volunteer.prototype, "skills", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Volunteer.prototype, "pastExperience", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Volunteer.prototype, "motivation", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Array)
+], Volunteer.prototype, "certificateLinks", void 0);
+exports.Volunteer = Volunteer = __decorate([
+    (0, mongoose_1.Schema)()
+], Volunteer);
+exports.VolunteerSchema = mongoose_1.SchemaFactory.createForClass(Volunteer);
+//# sourceMappingURL=volunteer.schema.js.map
---
+++ backend/src/volunteer/volunteer.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, ObjectId } from 'mongoose';
+
+export type VolunteerDocument = Volunteer & Document;
+
+@Schema()
+export class Volunteer {
+  @Prop({ required: true }) // Mongoose automatically assigns an ID
+  _id: string;
+
+  @Prop({ required: true })
+  userId: ObjectId; // Reference to the user document (for email association)
+
+  @Prop() // Exclude password from volunteer profile for security
+  profilePhoto: string; // URL or path to profile photo
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  phoneNo: string;
+
+  @Prop()
+  city: string;
+
+  @Prop()
+  state: string;
+
+  @Prop()
+  zipcode: string;
+
+  @Prop()
+  daysOfWeekAvailable: string[]; // Array of available days
+
+  @Prop()
+  skills: string[]; // Array of skills
+
+  @Prop()
+  pastExperience: string;
+
+  @Prop()
+  motivation: string;
+
+  @Prop()
+  certificateLinks: string[]; // Array of links to certificates
+}
+
+export const VolunteerSchema = SchemaFactory.createForClass(Volunteer);
